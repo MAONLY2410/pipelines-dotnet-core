b4a63a346da335c4a6fb26c93301037ed33c48b0
Máximo Castellón <bymynimonya@gmail.com>
2025-08-08 09:29:29 -0600

    Add files via upload

---
+++ DemoTests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DemoTests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void AlwaysPasses()
+        {
+            Assert.True(true);
+        }
+    }
+}
